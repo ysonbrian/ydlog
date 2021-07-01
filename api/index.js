0c96583e0034ac192a286f42573b509c0f617e39
ysonbrian <znlsakxls@gmail.com>
2021-07-01 22:59:39 +0900

    MongoDB connection

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-app.use('/', (req, res) => {
-  console.log('hey this is main url');
-});
+dotenv.config();
 
-app.listen('5000', () => {
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
+
+app.listen('6000', () => {
   console.log('Backend is running');
 });
