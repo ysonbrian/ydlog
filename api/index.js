4b0fb1990616198b8c080e9cac60b8387749e3bb
ysonbrian <znlsakxls@gmail.com>
2021-07-02 19:28:18 +0900

    User CRUD added

--- api/index.js
+++ api/index.js
@@ -3,12 +3,14 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
 
 dotenv.config();
 
 //Postman 사용하기 위해 express.json()을 사용함
 // request json..
 app.use(express.json());
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -19,6 +21,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 
 app.listen('6000', () => {
   console.log('Backend is running');
